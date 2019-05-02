899cc7d1413deebcd67d63a4f4eb4a7553d49535
Clarice Groeneveld <clari.groeneveld@gmail.com>
2019-05-02 16:41:37 -0300

    add install file

---
+++ fresh_install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+cd ~/Downloads
+
+#-- Instalação do R (v. 3.6)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/"
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install r-base r-base-dev
+
+#-- Bibliotecas do Sistema
+sudo apt-get install openssl libcurl4 libcurl4-openssl-dev ssh htop default-jdk gdebi libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
+###################################### 
+# Softwares
+###################################### 
+# RStudio v. 1.2
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1335-amd64.deb
+sudo gdebi rstudio-1.2.1335-amd64.deb
+
+# RStudio server v. 1.2
+sudo apt-get install gdebi-core
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.2.1335-amd64.deb
+sudo gdebi rstudio-server-1.2.1335-amd64.deb
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+bash Anaconda3-2019.03-Linux-x86_64.sh
+
+# Diversos (Spotify, Gimp, Inkscape)
+sudo snap install spotify gimp inkscape
+
+# Mendeley v. 1.19.5
+wget https://desktop-download.mendeley.com/download/apt/pool/main/m/mendeleydesktop/mendeleydesktop_1.19.5-stable_amd64.deb
+sudo gdebi mendeleydesktop_1.19.5-stable_amd64.deb
+
+# Nextcloud
+sudo add-apt-repository ppa:nextcloud-devs/client
+sudo apt-get install nextcloud-client
+
+###################################### 
+# Pacotes do R
+###################################### 
+Rscript -e 'install.packages("BiocManager", repos="https://cran.rstudio.com")
+BiocManager::install("tidyverse", "tinytex", "caret", "RTN", "DESeq2", "TCGAbiolinks", "EBImage", "keras", "RedeR")'
+
+
